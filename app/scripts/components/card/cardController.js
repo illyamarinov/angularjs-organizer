3e568439434ba73bb65ff4101fb867c534f5fd48
Ilya Marinov <ilya.marinov@hiqo-solutions.com>
2018-10-11 15:50:50 +0300

    adding edit and update card function, working on blur event

--- app/scripts/components/card/cardController.js
+++ app/scripts/components/card/cardController.js
@@ -1,3 +1,22 @@
-app.controller('cardController', function() {
-  
-});
+app.controller('cardController', cardController);
+
+cardController.$inject = ['$scope', 'cardService'];
+
+function cardController($scope, cardService) {
+
+  this.editCard = function(card) {
+    this.isEditing = true;
+    this.editingCard = angular.copy(card);
+  }
+
+  this.updateCard = function(card) {
+    const data = { title: card.title, description: card.description };
+    cardService
+      .updateCard(card._id, data)
+      .then(function() {
+        $scope.$parent.$parent.getCards();
+      });
+    this.editingCard = null;
+    this.isEditing = false;
+  }
+};
